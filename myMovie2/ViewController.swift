ce7cc122d1f2d884b68c575b493a2330ce3d7d9c
Mickey English <mickeyenglish@Mickeys-MacBook-Pro.local>
2018-03-26 00:44:28 +0100

    user authentication working with firebase

--- myMovie2/ViewController.swift
+++ myMovie2/ViewController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,46 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func loginButton(_ sender: UIButton) {
+        if emailInput.text != "" && passwordInput.text != "" {
+            if segmentControl.selectedSegmentIndex == 0 {
+                
+                // login with firebase
+                Auth.auth().signIn(withEmail: emailInput.text!,  password: passwordInput.text!, completion: { (user, error)  in
+                    if user != nil {
+                        // user sucessfully signs in
+                        print("Sucessfully signed in!")
+                    } else {
+                        
+                        if let myError = error?.localizedDescription {
+                            print(myError)
+                        } else {
+                            print("Error")
+                        }
+                        
+                    }
+                    
+                })
+            } else {
+                //signUp
+                Auth.auth().createUser(withEmail: emailInput.text!, password: passwordInput.text!, completion: { (user, error) in
+                    
+                    if user != nil {
+                        print("Sucessfully signed up!")
+                    } else {
+                        
+                        if let myError = error?.localizedDescription {
+                            print(myError)
+                        } else {
+                            print("Error")
+                        }
+                        
+                    }
+                    
+                })
+            }
+        }
+    }
+    
 }
 
